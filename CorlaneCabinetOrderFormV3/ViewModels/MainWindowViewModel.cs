233149053828ef46a04e739a76b9fdc540c90174
corlane2 <corlane2@OFFICE2>
2026-02-13 11:06:33 -0500

    Got rid of commented-out section in ConvertDimensions

--- CorlaneCabinetOrderFormV3/ViewModels/MainWindowViewModel.cs
+++ CorlaneCabinetOrderFormV3/ViewModels/MainWindowViewModel.cs
@@ -14,7 +14,7 @@ namespace CorlaneCabinetOrderFormV3.ViewModels;
 
 public partial class MainWindowViewModel : ObservableValidator
 {
-    public string AppTitle { get; } = "Corlane Cabinet Order Form - Version 3.0.1.17";
+    public string AppTitle { get; } = "Corlane Cabinet Order Form - Version 3.0.1.18";
 
     private readonly ICabinetService _cabinet_service;
 
