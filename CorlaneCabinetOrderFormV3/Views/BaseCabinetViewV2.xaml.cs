2a9f545e1a5a57f664aea76e91818327c0ab3971
corlane2 <corlane2@OFFICE2>
2026-04-15 15:54:30 -0400

    Messing with a new BaseCabinetViewV2.

---
+++ CorlaneCabinetOrderFormV3/Views/BaseCabinetViewV2.xaml.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using CorlaneCabinetOrderFormV3.ViewModels;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace CorlaneCabinetOrderFormV3.Views;
+
+public partial class BaseCabinetViewV2 : UserControl
+{
+    public BaseCabinetViewV2()
+    {
+        InitializeComponent();
+    }
+
+    private void TextBoxGotFocus(object sender, RoutedEventArgs e)
+    {
+        if (sender is TextBox textBox)
+        {
+            Dispatcher.BeginInvoke(() => textBox.SelectAll());
+        }
+
+        e.Handled = true;
+    }
+}
