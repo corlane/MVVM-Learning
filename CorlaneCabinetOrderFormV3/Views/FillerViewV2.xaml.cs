47cd68cbf55190e025f4c8d28aa5be75fb6d3678
corlane2 <corlane2@OFFICE2>
2026-04-16 12:02:39 -0400

    Filler and Panel V2 views added.

---
+++ CorlaneCabinetOrderFormV3/Views/FillerViewV2.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace CorlaneCabinetOrderFormV3.Views
+{
+    /// <summary>
+    /// Interaction logic for FillerViewV2.xaml
+    /// </summary>
+    public partial class FillerViewV2 : UserControl
+    {
+        public FillerViewV2()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBoxGotFocus(object sender, RoutedEventArgs e)
+        {
+            if (sender is TextBox textBox)
+                textBox.SelectAll();
+        }
+    }
+}
---
+++ CorlaneCabinetOrderFormV3/Views/PanelViewV2.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace CorlaneCabinetOrderFormV3.Views
+{
+    /// <summary>
+    /// Interaction logic for PanelViewV2.xaml
+    /// </summary>
+    public partial class PanelViewV2 : UserControl
+    {
+        public PanelViewV2()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBoxGotFocus(object sender, RoutedEventArgs e)
+        {
+            if (sender is TextBox textBox)
+                textBox.SelectAll();
+        }
+    }
+}
