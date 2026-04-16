b71b67bde08e24c3088a8b975bcaab012758791b
corlane2 <corlane2@OFFICE2>
2026-04-16 12:09:13 -0400

    Defaults V2 view added.

---
+++ CorlaneCabinetOrderFormV3/Views/DefaultSettingsViewV2.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace CorlaneCabinetOrderFormV3.Views
+{
+    /// <summary>
+    /// Interaction logic for DefaultSettingsViewV2.xaml
+    /// </summary>
+    public partial class DefaultSettingsViewV2 : UserControl
+    {
+        public DefaultSettingsViewV2()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBoxGotFocus(object sender, RoutedEventArgs e)
+        {
+            if (sender is TextBox textBox)
+                textBox.SelectAll();
+        }
+    }
+}
