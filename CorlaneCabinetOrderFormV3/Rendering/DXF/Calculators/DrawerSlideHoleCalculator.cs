8fde16aa58af891a4ce528e080989f04cca5df1d
corlane2 <corlane2@OFFICE2>
2026-05-22 14:59:31 -0400

    Added Accuride hole pattern. Looks good, but need to double check!

--- CorlaneCabinetOrderFormV3/Rendering/DXF/Calculators/DrawerSlideHoleCalculator.cs
+++ CorlaneCabinetOrderFormV3/Rendering/DXF/Calculators/DrawerSlideHoleCalculator.cs
@@ -1,143 +1,30 @@
-﻿//using CorlaneCabinetOrderFormV3.Models;
-//using CorlaneCabinetOrderFormV3.Rendering.V4.Core;
-//using System.Diagnostics;
-
-//namespace CorlaneCabinetOrderFormV3.Rendering.V4.Calculators;
-
-//internal class DrawerSlideHoleCalculator
-//{
-//    internal static List<(double x, double y, double radius)> Compute(BaseCabinetModel b, JoineryConfig joinery, double materialThickness34)
-//    {
-//        var holes = new List<(double, double, double)>();
-//        var baseCab = b;
-//        var dim = BaseCabinetDimensions.From(baseCab);
-
-//        double mt34 = materialThickness34;
-
-//        double radius = 0.19685 / 2.0; // 5mm diameter
-//        string slideType = baseCab.DrwStyle;
-//        double slidePositionX = 0;
-//        double patternHeightAboveStretcher = 0;
-
-//        if (baseCab.Style == CabinetStyles.Base.Standard && baseCab.DrwCount == 1)
-//        {
-//            double openingHeight = dim.Opening1Height;
-
-//            if (slideType.Contains("Blum"))
-//            {
-//                slidePositionX = mt34 + openingHeight;
-//                patternHeightAboveStretcher = 1.4961;
-
-//                slidePositionX -= patternHeightAboveStretcher;
-
-//                if (baseCab.DrillSlideHolesOpening1)
-//                {
-//                    ApplyBlumHolePattern(holes, dim, radius, slidePositionX);
-//                }
-//            }
-//        }
-
-//        if (baseCab.Style == CabinetStyles.Base.Drawer)
-//        {
-//            double[] openingHeight = [dim.Opening1Height, dim.Opening2Height, dim.Opening3Height, dim.Opening4Height];
-//            bool[] drillSlideHolesForOpening = [baseCab.DrillSlideHolesOpening1, baseCab.DrillSlideHolesOpening2, baseCab.DrillSlideHolesOpening3, baseCab.DrillSlideHolesOpening4];
-
-//            for (int openingIndex = 0; openingIndex < baseCab.DrwCount; openingIndex++)
-//            {
-//                if (slideType.Contains("Blum"))
-//                {
-//                    patternHeightAboveStretcher = 1.4961;
-
-//                    slidePositionX += mt34 + openingHeight[openingIndex];
-
-//                    if (drillSlideHolesForOpening[openingIndex])
-//                    {
-//                        ApplyBlumHolePattern(holes, dim, radius, slidePositionX - patternHeightAboveStretcher);
-//                    }
-//                }
-//            }
-//        }
-
-//        return holes;
-//    }
-
-
-//    private static void ApplyBlumHolePattern(List<(double, double, double)> holes, BaseCabinetDimensions dim, double radius, double slidePositionX)
-//    {
-//        double hole1Y = 0.3937;
-//        double hole2Y = 1.4567;
-//        double hole3Y = 2.7165;
-//        double hole4Y = 0;
-//        double hole5Y = 0;
-
-//        holes.Add((slidePositionX, hole1Y, radius));
-//        holes.Add((slidePositionX, hole2Y, radius));
-
-//        if (dim.Depth >= 24)
-//        {
-//            hole4Y = hole2Y + (224 / 25.4); // Convert mm to inches
-//            hole5Y = hole4Y + (256 / 25.4);
-//        }
-//        else if (dim.Depth >= 21 && dim.Depth < 24)
-//        {
-//            hole4Y = hole2Y + (224 / 25.4);
-//            hole5Y = hole4Y + (192 / 25.4);
-//        }
-//        else if (dim.Depth >= 18 && dim.Depth < 21)
-//        {
-//            hole4Y = hole2Y + (128 / 25.4);
-//            hole5Y = hole4Y + (192 / 25.4);
-//        }
-//        else if (dim.Depth >= 15 && dim.Depth < 18)
-//        {
-//            hole4Y = hole2Y + (128 / 25.4);
-//            hole5Y = hole4Y + (96 / 25.4);
-//        }
-//        else if (dim.Depth >= 12 && dim.Depth < 15)
-//        {
-//            hole3Y = hole2Y + (96 / 25.4);
-//            hole4Y = hole3Y + (96 / 25.4);
-//            holes.Add((slidePositionX, hole2Y, radius));
-//            holes.Add((slidePositionX, hole3Y, radius));
-//            holes.Add((slidePositionX, hole4Y, radius));
-//        }
-
-//        if (dim.Depth >= 15)
-//        {
-//            holes.Add((slidePositionX, hole3Y, radius));
-//            holes.Add((slidePositionX, hole4Y, radius));
-//            holes.Add((slidePositionX, hole5Y, radius));
-//        }
-//    }
-
-//    // Cab depth - back thickness - 5/8" (for 3/4" back). 
-//}
-
-
-
-
-
-
-
-using CorlaneCabinetOrderFormV3.Models;
+﻿using CorlaneCabinetOrderFormV3.Models;
 using CorlaneCabinetOrderFormV3.Rendering.V4.Core;
 
 namespace CorlaneCabinetOrderFormV3.Rendering.V4.Calculators;
 
 internal static class DrawerSlideHoleCalculator
 {
+    private const double MmToInch = 1.0 / 25.4;
+
     // 5mm diameter hole
     private const double HoleRadius = 0.19685 / 2.0;
 
-    // Blum slide pattern offset from bottom of opening
-    private const double PatternHeightAboveStretcher = 1.4961;
+    // Slide pattern offset from bottom of opening
+    private const double BlumPatternHeightAboveStretcher = 1.4961;
+    private const double AccuridePatternHeightAboveStretcher = 1.4961; // TODO NEED THE ACTUAL NUMBER
 
     // Fixed Y-offsets (inches)
     private const double BlumHole1Y = 0.3937;
     private const double BlumHole2Y = 1.4567;
     private const double BlumHole3Y_Default = 2.7165;
 
-    private const double MmToInch = 1.0 / 25.4;
+    private const double AccurideInsetFromFront = 2;
+    private const double AccurideHole1Y = (35 + AccurideInsetFromFront) * MmToInch;
+    private const double AccurideHole2Y = (AccurideHole1Y + 224) * MmToInch;
+    
+
+
 
     internal static List<(double x, double y, double radius)> Compute(
         BaseCabinetModel baseCab,
@@ -145,9 +32,6 @@ internal static class DrawerSlideHoleCalculator
         double materialThickness34)
     {
         var holes = new List<(double, double, double)>();
-
-        if (!baseCab.DrwStyle.Contains("Blum", StringComparison.OrdinalIgnoreCase))
-            return holes;
 
         var dim = BaseCabinetDimensions.From(baseCab);
 
@@ -186,9 +70,17 @@ internal static class DrawerSlideHoleCalculator
             }
 
             runningX += mt34 + openingHeights[i];
-            double slidePositionX = runningX - PatternHeightAboveStretcher;
+            double slidePositionX = runningX - BlumPatternHeightAboveStretcher;
 
-            ApplyBlumHolePattern(holes, dim, slidePositionX);
+            if (baseCab.DrwStyle.Contains("Blum"))
+            {
+                ApplyBlumHolePattern(holes, dim, slidePositionX);
+            }
+
+            else
+            {
+                ApplyAccurideHolePattern(holes, dim, slidePositionX);
+            }
         }
     }
 
@@ -247,5 +139,68 @@ internal static class DrawerSlideHoleCalculator
         {
             holes.Add((slidePositionX, hole4Y, HoleRadius));
         }
+    }
+
+    private static void ApplyAccurideHolePattern(
+        List<(double x, double y, double radius)> holes,
+        BaseCabinetDimensions dim,
+        double slidePositionX)
+    {
+        double hole1Y = AccurideHole1Y;
+        double hole2Y = AccurideHole2Y;
+        double hole3Y = 0;
+        double hole4Y = 0;
+
+        holes.Add((slidePositionX, hole1Y, HoleRadius));
+        holes.Add((slidePositionX, hole2Y, HoleRadius));
+
+        // 16" slide:
+        if (dim.Depth >= 17 && dim.Depth < 18.999)
+        {
+            hole3Y = hole1Y + (224 * MmToInch);
+            holes.Add((slidePositionX, hole3Y, HoleRadius));
+        }
+
+        // 18" slide:
+        if (dim.Depth >= 19 && dim.Depth < 20.999)
+        {
+            hole3Y = hole1Y + (352 * MmToInch);
+            holes.Add((slidePositionX, hole3Y, HoleRadius));
+        }
+
+        // 20" slide:
+        if (dim.Depth >= 21 && dim.Depth < 22.999)
+        {
+            hole3Y = hole1Y + (416 * MmToInch);
+            holes.Add((slidePositionX, hole3Y, HoleRadius));
+        }
+
+        // 22" slide:
+        if (dim.Depth >= 23 && dim.Depth < 24.999)
+        {
+            hole3Y = hole1Y + (352 * MmToInch);
+            hole4Y = hole1Y + (448 * MmToInch);
+            holes.Add((slidePositionX, hole3Y, HoleRadius));
+            holes.Add((slidePositionX, hole4Y, HoleRadius));
+        }
+
+        // 24" slide:
+        if (dim.Depth >= 25 && dim.Depth < 26.999)
+        {
+            hole3Y = hole1Y + (352 * MmToInch);
+            hole4Y = hole1Y + (480 * MmToInch);
+            holes.Add((slidePositionX, hole3Y, HoleRadius));
+            holes.Add((slidePositionX, hole4Y, HoleRadius));
+        }
+
+        // 26" & 28" slides:
+        if (dim.Depth >= 27)
+        {
+            hole3Y = hole1Y + (352 * MmToInch);
+            hole4Y = hole1Y + (544 * MmToInch);
+            holes.Add((slidePositionX, hole3Y, HoleRadius));
+            holes.Add((slidePositionX, hole4Y, HoleRadius));
+        }
+
     }
 }
