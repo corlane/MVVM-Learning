d235a57c4bb88ce4d5131dbbda9ed93dabb6677c
corlane2 <corlane2@OFFICE2>
2026-06-08 11:43:59 -0400

    No change from last, just added new version number to Changelog

--- CorlaneCabinetOrderFormV3/ViewModels/MainWindow/MainWindowViewModel.cs
+++ CorlaneCabinetOrderFormV3/ViewModels/MainWindow/MainWindowViewModel.cs
@@ -12,7 +12,7 @@ namespace CorlaneCabinetOrderFormV3.ViewModels;
 
 public partial class MainWindowViewModel : ObservableValidator
 {
-    public string AppTitle { get; } = "Corlane Cabinet Order Form - Version 3.1.0.7";
+    public string AppTitle { get; } = "Corlane Cabinet Order Form - Version 3.1.0.8";
 
     private readonly ICabinetService _cabinetService;
     private readonly AutoSaveService _autoSave;
