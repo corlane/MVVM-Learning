9595e2e721771a0358c62d9bb775f0ab592df31e
corlane2 <corlane2@OFFICE2>
2026-07-31 14:27:14 -0400

    Removed ability to enter # sign in Name textbox.

--- CorlaneCabinetOrderFormV3/Views/BaseCabinetFlyouts/BottomActionPanel.xaml.cs
+++ CorlaneCabinetOrderFormV3/Views/BaseCabinetFlyouts/BottomActionPanel.xaml.cs
@@ -1,6 +1,27 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
 namespace CorlaneCabinetOrderFormV3.Views.BaseCabinetFlyouts;
 
 public partial class BottomActionPanel : FlyoutUserControlBase
 {
     public BottomActionPanel() => InitializeComponent();
+
+    private void TextName_PreviewTextInput(object sender, TextCompositionEventArgs e)
+    {
+        // Block the # character as it is typed
+        if (e.Text.Contains('#'))
+            e.Handled = true;
+    }
+
+    private void TextName_Pasting(object sender, DataObjectPastingEventArgs e)
+    {
+        // Also block pasting text that contains #
+        if (e.DataObject.GetDataPresent(typeof(string)))
+        {
+            string text = (string) e.DataObject.GetData(typeof(string));
+            if (text != null && text.Contains('#'))
+                e.CancelCommand();
+        }
+    }
 }
