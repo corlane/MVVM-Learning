cc8231af486e9ea1b987218eb8f61760883a0cfe
corlane2 <corlane2@OFFICE2>
2026-09-03 13:16:14 -0400

    3.1.0.28  Initial commit.

--- CorlaneCabinetOrderFormV3/ViewModels/MainWindow/MainWindowViewModel.cs
+++ CorlaneCabinetOrderFormV3/ViewModels/MainWindow/MainWindowViewModel.cs
@@ -12,7 +12,7 @@ namespace CorlaneCabinetOrderFormV3.ViewModels;
 
 public partial class MainWindowViewModel : ObservableValidator
 {
-    public string AppTitle { get; } = "Corlane Cabinet Order Form - Version 3.1.0.27";
+    public string AppTitle { get; } = "Corlane Cabinet Order Form - Version 3.1.0.28";
 
     private readonly ICabinetService _cabinetService;
     private readonly AutoSaveService _autoSave;
