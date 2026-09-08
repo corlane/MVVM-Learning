58576afd6dc2f599a869b5ffc7d1aaa094c791d5
corlane2 <corlane2@OFFICE2>
2026-09-08 09:04:02 -0400

    Fixed Drawer Front Height and Opening Height boxes to properly show default dimension format. commit

--- CorlaneCabinetOrderFormV3/ViewModels/BaseCabinet/BaseCabinetViewModel.Layout.cs
+++ CorlaneCabinetOrderFormV3/ViewModels/BaseCabinet/BaseCabinetViewModel.Layout.cs
@@ -1,7 +1,6 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CorlaneCabinetOrderFormV3.Converters;
 using CorlaneCabinetOrderFormV3.Services;
-using System.Diagnostics;
 
 namespace CorlaneCabinetOrderFormV3.ViewModels;
 
@@ -20,18 +19,6 @@ public partial class BaseCabinetViewModel : ObservableValidator
             var input = BuildLayoutInputs();
             var result = CabinetLayoutCalculator.ComputeFromOpenings(input);
             ApplyLayoutResult(result);
-
-            //// Style-specific disable flags
-            //if (Style == Style1)
-            //    Opening1Disabled = DrwCount == 0;
-            //else if (Style == Style2)
-            //{
-            //    Opening1Disabled = DrwCount == 1;
-            //    if (DrwCount >= 2) { Opening1Disabled = false; Opening2Disabled = true; Opening3Disabled = true; }
-            //    if (DrwCount >= 3) Opening2Disabled = false;
-            //    if (DrwCount >= 4) Opening3Disabled = false;
-            //    Debug.WriteLine($"ResizeOpeningHeights: Style2, DrwCount={DrwCount}, Opening1Disabled={Opening1Disabled}, Opening2Disabled={Opening2Disabled}, Opening3Disabled={Opening3Disabled}");
-            //}
 
             ApplyDrawerFrontEqualization();
             UpdateDisabledFlags();
@@ -59,24 +46,15 @@ public partial class BaseCabinetViewModel : ObservableValidator
 
     private void ApplyLayoutResult(CabinetLayoutCalculator.LayoutResult r)
     {
-        //if (_activeInputProperty != nameof(OpeningHeight1)) OpeningHeight1 = r.Opening1.ToString();
-        //if (_activeInputProperty != nameof(OpeningHeight2)) OpeningHeight2 = r.Opening2.ToString();
-        //if (_activeInputProperty != nameof(OpeningHeight3)) OpeningHeight3 = r.Opening3.ToString();
-        //if (_activeInputProperty != nameof(OpeningHeight4)) OpeningHeight4 = r.Opening4.ToString();
-        //if (_activeInputProperty != nameof(DrwFrontHeight1)) DrwFrontHeight1 = r.DrwFront1.ToString();
-        //if (_activeInputProperty != nameof(DrwFrontHeight2)) DrwFrontHeight2 = r.DrwFront2.ToString();
-        //if (_activeInputProperty != nameof(DrwFrontHeight3)) DrwFrontHeight3 = r.DrwFront3.ToString();
-        //if (_activeInputProperty != nameof(DrwFrontHeight4)) DrwFrontHeight4 = r.DrwFront4.ToString();
-
         var active = _activeInputProperty;
-        if (active != nameof(OpeningHeight1)) OpeningHeight1 = r.Opening1.ToString();
-        if (active != nameof(OpeningHeight2)) OpeningHeight2 = r.Opening2.ToString();
-        if (active != nameof(OpeningHeight3)) OpeningHeight3 = r.Opening3.ToString();
-        if (active != nameof(OpeningHeight4)) OpeningHeight4 = r.Opening4.ToString();
-        if (active != nameof(DrwFrontHeight1)) DrwFrontHeight1 = r.DrwFront1.ToString();
-        if (active != nameof(DrwFrontHeight2)) DrwFrontHeight2 = r.DrwFront2.ToString();
-        if (active != nameof(DrwFrontHeight3)) DrwFrontHeight3 = r.DrwFront3.ToString();
-        if (active != nameof(DrwFrontHeight4)) DrwFrontHeight4 = r.DrwFront4.ToString();
+        if (active != nameof(OpeningHeight1)) OpeningHeight1 = FormatDimension(r.Opening1);
+        if (active != nameof(OpeningHeight2)) OpeningHeight2 = FormatDimension(r.Opening2);
+        if (active != nameof(OpeningHeight3)) OpeningHeight3 = FormatDimension(r.Opening3);
+        if (active != nameof(OpeningHeight4)) OpeningHeight4 = FormatDimension(r.Opening4);
+        if (active != nameof(DrwFrontHeight1)) DrwFrontHeight1 = FormatDimension(r.DrwFront1);
+        if (active != nameof(DrwFrontHeight2)) DrwFrontHeight2 = FormatDimension(r.DrwFront2);
+        if (active != nameof(DrwFrontHeight3)) DrwFrontHeight3 = FormatDimension(r.DrwFront3);
+        if (active != nameof(DrwFrontHeight4)) DrwFrontHeight4 = FormatDimension(r.DrwFront4);
     }
 
     private void ResizeDrwFrontHeights()
@@ -90,20 +68,6 @@ public partial class BaseCabinetViewModel : ObservableValidator
             var input = BuildLayoutInputs();
             var result = CabinetLayoutCalculator.ComputeFromDrawerFronts(input);
             ApplyLayoutResult(result);
-
-            //// Style-specific disable flags
-            //if (Style == Style1 && DrwCount == 1)
-            //{
-            //    Opening1Disabled = false;
-            //    DrwFront1Disabled = false;
-            //}
-            //else if (Style == Style2)
-            //{
-            //    if (DrwCount == 1) DrwFront1Disabled = true;
-            //    if (DrwCount >= 2) { DrwFront1Disabled = false; DrwFront2Disabled = true; }
-            //    if (DrwCount >= 3) { DrwFront2Disabled = false; DrwFront3Disabled = true; }
-            //    if (DrwCount >= 4) DrwFront3Disabled = false;
-            //}
 
             if (EqualizeBottomDrwFronts)
             {
@@ -123,61 +87,6 @@ public partial class BaseCabinetViewModel : ObservableValidator
         finally { _isResizing = false; }
     }
 
-    //private void ApplyDrawerFrontEqualization()
-    //{
-    //    if (_isResizing || _isMapping) return;
-    //    if (Style != Style2) return;
-    //    if (DrwCount <= 0) return;
-    //    if (!EqualizeAllDrwFronts && !EqualizeBottomDrwFronts) return;
-
-    //    double tkHeight = ConvertDimension.FractionToDouble(TKHeight);
-    //    if (!HasTK) tkHeight = 0;
-    //    double height = ConvertDimension.FractionToDouble(Height) - tkHeight;
-
-    //    double topReveal = ConvertDimension.FractionToDouble(TopReveal);
-    //    double bottomReveal = ConvertDimension.FractionToDouble(BottomReveal);
-    //    double gapWidth = ConvertDimension.FractionToDouble(GapWidth);
-
-    //    try
-    //    {
-    //        _isResizing = true; // _isResizing = true breaks this, because the openings won't resize
-
-    //        if (EqualizeAllDrwFronts)
-    //        {
-    //            //if (DrwCount <= 0) return;
-
-    //            //double each = CabinetLayoutCalculator.EqualizeAll(height, topReveal, bottomReveal, gapWidth, DrwCount);
-
-    //            //DrwFrontHeight1 = each.ToString();
-    //            //DrwFrontHeight2 = each.ToString();
-    //            //DrwFrontHeight3 = each.ToString();
-    //            //if (DrwCount >= 4) DrwFrontHeight4 = each.ToString();
-
-    //            double each = CabinetLayoutCalculator.EqualizeAll(height, topReveal, bottomReveal, gapWidth, DrwCount);
-
-    //            if (_activeInputProperty != nameof(DrwFrontHeight1)) DrwFrontHeight1 = each.ToString();
-    //            if (_activeInputProperty != nameof(DrwFrontHeight2)) DrwFrontHeight2 = each.ToString();
-    //            if (_activeInputProperty != nameof(DrwFrontHeight3)) DrwFrontHeight3 = each.ToString();
-    //            if (DrwCount >= 4 && _activeInputProperty != nameof(DrwFrontHeight4))
-    //                DrwFrontHeight4 = each.ToString();
-    //        }
-    //        else if (EqualizeBottomDrwFronts)
-    //        {
-    //            if (DrwCount <= 1) return;
-
-    //            double top = ConvertDimension.FractionToDouble(DrwFrontHeight1);
-    //            double eachBottom = CabinetLayoutCalculator.EqualizeBottom(height, topReveal, bottomReveal, gapWidth, DrwCount, top);
-
-    //            if (DrwCount >= 2) DrwFrontHeight2 = eachBottom.ToString();
-    //            if (DrwCount >= 3) DrwFrontHeight3 = eachBottom.ToString();
-    //            if (DrwCount >= 4) DrwFrontHeight4 = eachBottom.ToString();
-    //        }
-    //    }
-    //    finally
-    //    {
-    //        _isResizing = false;
-    //    }
-    //}
 
     private void ApplyDrawerFrontEqualization()
     {
@@ -340,5 +249,12 @@ public partial class BaseCabinetViewModel : ObservableValidator
             DrwFront2Disabled = true;
             DrwFront3Disabled = true;
         }
+    }
+    private string FormatDimension(double value)
+    {
+        string dimFormat = _defaults?.DefaultDimensionFormat ?? "Decimal";
+        return string.Equals(dimFormat, "Fraction", StringComparison.OrdinalIgnoreCase)
+            ? ConvertDimension.DoubleToFraction(value)
+            : value.ToString("0.####");
     }
 }
